c3e825d0ebb4180c7df16e0ae4a36bd36e6d8027
Ravi vardhan <ravivardhanbtechcse@gmail.com>
2023-04-14 15:44:03 +0530

    initial commit

---
+++ cssm/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse,request
+# Create your views here.
+
+def login(request):
+    return render(request,'login.html')
+
+def signup(request):
+    return render(request,'signup.html')
